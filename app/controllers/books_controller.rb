23da98cdb4473b1e921904698aa4c8ab0c527d71
Kappie <ghkapteijns@gmail.com>
2013-11-09 19:34:50 +0100

    CRUD actions for post model added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_filter :authorize, :except => [:index, :show]
 
   def index
     @books = Book.all
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,33 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to blog_path
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
