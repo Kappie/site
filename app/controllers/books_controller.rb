456dfbc5a8bd8cc7c8cd55760a0d6c0ef8ec42f4
Kappie <ghkapteijns@gmail.com>
2013-11-10 03:28:10 +0100

    finished admin layer! CRUD actions for books, posts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,4 +5,41 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.create(book_params)
+    redirect_to @book
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to @book
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to boeken_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :rating, :isbn,
+      :review, :abstract, :cover)
+  end
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Kapteijns::Application.routes.draw do
   match "/login", to: "sessions#new", via: "get"
   match "/logout", to: "sessions#destroy", via: "get"
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
